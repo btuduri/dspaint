697e52095a6ef751546bdaae663f07d4c224edc6
nightstalkerz <nightstalkerz@08a58f1c-9a32-11dd-9476-71c886a649bc>
2008-10-16 19:49:05 +0000

    Added some files.

---
+++ src/common.h
@@ -0,0 +1,14 @@
+
+// options for the current image
+// probably needs refactoring in milestone 0.2
+#typedef struct
+{
+	int mode,
+} OPTIONS;
+
+// settings for the current pen
+#typedef struct
+{
+	int colour,
+	int width,
+} PEN;
