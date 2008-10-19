cc7325ae25eccd671354afae07df2695f250e604
nightstalkerz <nightstalkerz@08a58f1c-9a32-11dd-9476-71c886a649bc>
2008-10-19 19:20:57 +0000

    Tagged milestone 0.1

---
+++ tags/dspaint-0.1/src/constants.h
@@ -0,0 +1,11 @@
+// information about the DS
+#define NDS_SCREEN_MAX_WIDTH   256
+#define NDS_SCREEN_MAX_HEIGHT  192
+
+// some about information
+#define DSP_ABOUT_COPYRIGHT    "Copyright 2008 Kwok-wai Kan, Peter Vousden"
+#define DSP_ABOUT_DESCRIPTION  "A paint program for the Nintendo DS."
+
+// draw modes
+#define DSP_MODE_PEN           0
+#define DSP_MODE_ERASER        1
---
+++ tags/dspaint-0.1/src/main.cpp
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------------
+
+	$Id: main.c,v 1.5 2007/10/23 00:46:29 wntrmute Exp $
+
+	Simple console print demo
+	-- dovoto
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "constants.h"
+
+using namespace std;
+
+// sets the screen to white
+// this is dependant on whichever screen is currently being set
+void clear()
+{
+	// get the max number of pixels
+	int max = NDS_SCREEN_MAX_WIDTH * NDS_SCREEN_MAX_HEIGHT;
+	
+	// set each individual pixel to white
+	for (int i = 0; i < max; i++)
+	{
+		VRAM_A[i] = RGB15(31, 31, 31);
+	}
+}
+
+// entry point
+int main(void)
+{
+	// this is the struct for when the DS touches the screen
+	touchPosition touchXY;
+
+	// always needs to have
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	// set the video mode
+	// default is black screen
+	videoSetMode(MODE_FB0);
+	vramSetBankA(VRAM_A_LCD);
+
+	// draw onto the bottom screen
+	lcdMainOnBottom();
+	
+	// clear the screen
+	clear();
+	
+	// loop for processing commands
+	while (true)
+	{
+		// this is to keep the DS stop wasting 100% CPU in the loop
+		swiWaitForVBlank();
+		
+		// get the position of the touch screen position
+		touchXY = touchReadXY();
+		
+		// set that pixel to black
+		VRAM_A[touchXY.px + touchXY.py * NDS_SCREEN_MAX_WIDTH] = RGB15(0, 0, 0);
+	}
+
+	return 0;
+}
