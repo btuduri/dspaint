918d7afd0c8a0f43175c7300ea77dace69dd66f4
nightstalkerz <nightstalkerz@08a58f1c-9a32-11dd-9476-71c886a649bc>
2008-10-24 22:02:52 +0000

    Refactored some code. Added some documentation. Removed Common.h.

--- trunk/src/Canvas.h
+++ trunk/src/Canvas.h
@@ -1,17 +1,47 @@
-#ifndef CANVAS
-#define CANVAS
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include "Constants.h"
+#include "Dimension.h"
 
 namespace DSPaint
 {
     class Canvas
     {
         private:
-        int backgroundColour;
+			DIMENSION dimension;
+			int backgroundColour;
 
         public:
-        int* GetDimensions(int* array);
-        int GetBackgroundColour();
-        void SetBackgroundColour(int backgroundColour);
+			/**
+			 * Creates a new canvas.
+			 * @width The width of the canvas.
+			 * @height The height of the canvas.
+			 */
+			Canvas(int width = NDS_SCREEN_MAX_WIDTH, int height = NDS_SCREEN_MAX_HEIGHT);
+
+			/**
+			 * Gets the dimensions of this canvas.
+			 */
+			DIMENSION GetDimensions();
+
+			/**
+			 * Gets the background colour of this canvas.
+			 */
+			int GetBackgroundColour();
+
+			/**
+			 * Sets the background colour of this canvas.
+			 * @backgroundColour The background colour of this canvas.
+			 */
+			void SetBackgroundColour(int backgroundColour);
+
+			/**
+			 * Sets the screen to white.
+			 * This is dependant on whichever screen is currently being set.
+			 */
+			void Clear();
     };
 }
+
 #endif
--- trunk/src/IPen.h
+++ trunk/src/IPen.h
@@ -1,3 +1,6 @@
+#ifndef IPEN_H
+#define IPEN_H
+
 namespace DSPaint
 {
 	class IPen
@@ -7,9 +10,28 @@ namespace DSPaint
 			int _colour;
 
 		public:
+			/**
+			 * Gets the width of the pen.
+			 */
 			virtual int GetWidth() = 0;
+
+			/**
+			 * Sets the width of the pen.
+			 * @width The width to set.
+			 */
 			virtual void SetWidth(int width) = 0;
+
+			/**
+			 * Gets the colour of the pen.
+			 */
 			virtual int GetColour() = 0;
+
+			/**
+			 * Sets the colour of the pen.
+			 * @colour The colour to set.
+			 */
 			virtual void SetColour(int colour) = 0;
 	};
 }
+
+#endif
