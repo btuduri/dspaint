bade8f3062513ba03c4519cfc7744b3e6b9e357e
nightstalkerz <nightstalkerz@08a58f1c-9a32-11dd-9476-71c886a649bc>
2008-10-25 19:57:36 +0000

    Moved modes into Mode.h. Created Options class.

--- src/Constants.h
+++ src/Constants.h
@@ -5,7 +5,3 @@
 // some about information
 #define DSP_ABOUT_COPYRIGHT    "Copyright 2008 Kwok-wai Kan, Peter Vousden"
 #define DSP_ABOUT_DESCRIPTION  "A paint program for the Nintendo DS."
-
-// draw modes
-#define DSP_MODE_PEN           0
-#define DSP_MODE_ERASER        1
