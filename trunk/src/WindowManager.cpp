ba81f840447a0ded8a9729b59c65f6a401fee1cd
nightstalkerz <nightstalkerz@08a58f1c-9a32-11dd-9476-71c886a649bc>
2008-11-23 21:30:49 +0000

    Added message dialog with variable amount of buttons.

--- trunk/src/WindowManager.cpp
+++ trunk/src/WindowManager.cpp
@@ -10,20 +10,23 @@ namespace DSPaint
         Woopsi::startup();
 
         // Create screen
-        AmigaScreen* screen = new AmigaScreen("Hello World Screen", Gadget::GADGET_DRAGGABLE, AmigaScreen::AMIGA_SCREEN_SHOW_DEPTH | AmigaScreen::AMIGA_SCREEN_SHOW_FLIP);
+        Screen* screen = new Screen("Hello World Screen", 0);
         woopsiApplication->addGadget(screen);
 
         // Add window
-        AmigaWindow* window = new AmigaWindow(0, 13, 256, 179, "Hello World Window", Gadget::GADGET_DRAGGABLE, AmigaWindow::AMIGA_WINDOW_SHOW_CLOSE | AmigaWindow::AMIGA_WINDOW_SHOW_DEPTH);
+        Window* window = new Window(0, 13, 256, 179, "Hello World Window", 0);
         screen->addGadget(window);
 
+        //button1 = new Button(100, 100, 80, 20, "OK");
+       // window->addGadget(button1);
+
         // Get available area within window
-        Gadget::Rect rect;
-        window->getClientRect(rect);
+        //Gadget::Rect rect;
+       // window->getClientRect(rect);
 
         // Add textbox
-        TextBox* textbox = new TextBox(rect.x, rect.y, rect.width, rect.height, "Hello World!");
-        window->addGadget(textbox);
+       // TextBox* textbox = new TextBox(rect.x, rect.y, rect.width, rect.height, "Hello World!");
+        //window->addGadget(textbox);
 
         // Ensure Woopsi can draw itself
         enableDrawing();
@@ -38,8 +41,40 @@ namespace DSPaint
         Woopsi::shutdown();
     }
 
-    void WindowManager::ShowMessageBox()
+    int WindowManager::ShowMessageBox(const char* message, int args, ...)
     {
+    	Woopsi::startup();
 
+    	Screen* screen = new Screen("", 0);
+        woopsiApplication->addGadget(screen);
+
+        Window* window = new Window(0, 0, NDS_SCREEN_MAX_WIDTH, NDS_SCREEN_MAX_HEIGHT, "", 0);
+        screen->addGadget(window);
+
+        Gadget::Rect rect;
+        window->getClientRect(rect);
+
+        MultiLineTextBox* textbox = new MultiLineTextBox(4, 4, 152, NDS_SCREEN_MAX_HEIGHT - 8, message, 1, 16);
+        window->addGadget(textbox);
+
+		// dynamically add buttons
+    	va_list buttons;
+    	int height = 4;
+
+    	va_start(buttons, args);
+    	for (int i = 0; i < args; i++)
+    	{
+    		Button *b = new Button(160, height, 92, 20, va_arg(buttons, char*));
+    		window->addGadget(b);
+
+    		height += 22;
+    	}
+    	va_end(buttons);
+
+        enableDrawing();
+
+        draw();
+
+		return 0;
     }
 }
